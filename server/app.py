e219ca72ba5c38d1a7be66f96b9864464c29adfc
tgartland89 <thomas.gartland@gmail.com>
2023-07-13 19:18:33 -0600

    fixed lab

--- server/app.py
+++ server/app.py
@@ -16,6 +16,7 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
 
     def get(self):
@@ -23,7 +24,6 @@ class Plants(Resource):
         return make_response(jsonify(plants), 200)
 
     def post(self):
-
         data = request.get_json()
 
         new_plant = Plant(
@@ -37,41 +37,38 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
+
 api.add_resource(Plants, '/plants')
 
-class PlantByID(Resource):
 
+class PlantByID(Resource):
     def get(self, id):
-        plant = Plant.query.get(id)
-        if plant:
-            return make_response(jsonify(plant.to_dict()), 200)
-        else:
-            return make_response(jsonify({"message": "Plant not found"}), 404)
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(plant), 200)
 
     def patch(self, id):
-        plant = Plant.query.get(id)
-        if plant:
-            data = request.get_json()
-            plant.is_in_stock = data.get('is_in_stock', plant.is_in_stock)
-            db.session.commit()
-            return make_response(jsonify(plant.to_dict()), 200)
-        else:
-            return make_response(jsonify({"message": "Plant not found"}), 404)
+        data = request.get_json()
+
+        plant = Plant.query.filter_by(id=id).first()
+
+        for attr in data:
+            setattr(plant, attr, data[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
 
     def delete(self, id):
-        plant = Plant.query.get(id)
-        if plant:
-            db.session.delete(plant)
-            db.session.commit()
-            return make_response(jsonify({"message": "Plant deleted"}), 200)
-        else:
-            return make_response(jsonify({"message": "Plant not found"}), 404)
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
 
-
+        return make_response('', 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-        
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
